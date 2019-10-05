e6f9666c36d95d7d4726582fe902dbac34107e84
ArakiTakaki <araki_takaki@yahoo.co.jp>
2019-10-06 01:42:42 +0900

    update to init.sh

--- init.sh
+++ init.sh
@@ -5,5 +5,5 @@ read PROJECT_NAME
 #結果を表示
 echo $PROJECT_NAME
 
-cat README.md | sed "s/##PROJECT_NAME##/${PROJECT_NAME}/g" > README.md
-cat package.json | sed "s/##PROJECT_NAME##/${PROJECT_NAME}/g" > package.json
+sed -i '' "s/##PROJECT_NAME##/${PROJECT_NAME}/g" ./README.md
+sed -i '' "s/##PROJECT_NAME##/${PROJECT_NAME}/g" ./package.json
